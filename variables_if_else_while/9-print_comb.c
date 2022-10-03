1f8a7c0e84e98c18b24e4d14a8127336474e4561
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 08:37:32 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,10 +11,14 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i <= 9 ; i++)
-		putchar (i);
-		putchar (',');
-		putchar (' ');
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
+		putchar(',');
+		putchar(' ');
+	}
 	putchar ('\n');
 
 	return (0);
