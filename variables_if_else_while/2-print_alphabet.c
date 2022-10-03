787576446e8b250502e6bb8fb17e7a97390d9e88
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 03:49:41 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet
+ *@void: Null value
+ *
+ * Return: Zero value
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i ='a' ; i <= 'z' ; i++)
+		putchar (i);
+
+	putchar ('\n');
+
+	return (0);
+}
