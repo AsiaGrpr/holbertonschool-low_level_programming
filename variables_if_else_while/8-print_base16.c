c450d0a66250754c37b5b89a810e553ae6a60f1d
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 07:05:52 -0700

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
-  1 #include <stdio.h>
-  2 /**
-  3  * main - print the lowercase numbers of base 16
-  4  *@void: Null value
-  5  *
-  6  * Return: Zero value
-  7 */
-  8 
-  9 int main(void)
- 10 {
- 11     char i;
- 12 
- 13     for (i = 0 ; i <= 9 ; i++)
- 14         putchar (i);
- 15 
- 16     for (i = 'a' ; i <= 'f' ; i++)
- 17         putchar (i);
- 18 
- 19     putchar ('\n');
- 20 
- 21     return (0);
- 22 }
+ #include <stdio.h>
+/**
+* main - print the lowercase numbers of base 16
+*@void: Null value
+*
+* Return: Zero value
+*/
+ 
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i <= 9 ; i++)
+        putchar (i);
+
+    for (i = 'a' ; i <= 'f' ; i++)
+        putchar (i);
+
+    putchar ('\n');
+
+    return (0);
+}
