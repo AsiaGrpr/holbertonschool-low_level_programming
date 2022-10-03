9d2d527107f63f0044692ece08a0e3ddd5b77f7c
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 04:09:47 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet
+ *@void: Null value
+ *
+ * Return: Zero value
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	if (i != 'q' && i != 'e')
+		putchar (i);
+
+
+	putchar ('\n');
+
+	return (0);
+}
