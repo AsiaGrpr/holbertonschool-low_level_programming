ce85765e0620cb0b0bbe3ef1f7da66917ba82036
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 08:21:57 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all digit numbers separated by a space ans coma
+ *@void: Null value
+ *
+ * Return: Zero value
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar (i', ');
+
+	putchar ('\n');
+
+	return (0);
+}
