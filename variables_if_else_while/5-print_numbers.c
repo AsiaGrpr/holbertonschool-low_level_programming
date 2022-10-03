9be8fe01379a9f1b08cb4ec989dba49cd5e7c9a5
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-03 04:19:32 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print digit numbers from '0' to '10'
+ *@void: Null value
+ *
+ * Return: Zero value
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '10' ; i++)
+		putchar (i);
+
+	putchar ('\n');
+
+	return (0);
+}
