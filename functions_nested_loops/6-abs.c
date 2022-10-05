d406f5f2613f7e3151f136035d2a7696b0590ffb
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 08:25:51 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_abs - give the absolute number
+ *@int: number to check
+ *
+ * Return: Null value
+*/
+
+int _abs(int)
+{
+	if (int > 0)
+	{
+		_putchar('%d', int);
+		return (0);
+	}
+	else if (int == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		z = 0 - int
+		_putchar('%d', z);
+		return (0);
+	}
+}
