5ad1809addfc2f7b5cad8e6d48fdb583dffaa5f3
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 11:53:49 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - print the result of an addition of two integers
+ *@a: integer
+ *@b: integer
+ *
+ * Return: result of addition
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
