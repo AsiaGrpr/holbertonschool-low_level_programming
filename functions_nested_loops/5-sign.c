0d47fd1a579ea9b281e35a0b6bfc0ecae17b1d8e
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 06:24:36 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_sign - check if a number is +, - or zero and print a sign
+ *@n: the number to check
+ *
+ * Return: 1 when its postive, 0 if its zero or -1 if its negative
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
