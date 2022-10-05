155042e89b81638928d5d017dfda1daa9e9e695e
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 09:43:01 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_last_digit - print the last digit of a number
+ *@n: number to check
+ *
+ * Return: value of the digit
+*/
+int print_last_digit(int n)
+{
+	int n, digit;
+
+	digit = n % 10;
+
+	return (digit);
+}
