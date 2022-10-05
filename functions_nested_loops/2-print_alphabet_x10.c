a7f01a3a324e708ec2df4d24f879cdb92a8d2c15
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 04:49:30 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0; i < 10; i++)
 	{
-	    char a;
+	char a;
 
-	    for (a = 'a' ; a <= 'z' ; a++)
-				_putchar (a);
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
 
-	    _putchar ('\n');
+	_putchar('\n');
 	}
 }
