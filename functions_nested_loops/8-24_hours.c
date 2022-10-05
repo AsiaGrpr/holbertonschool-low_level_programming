c4a5572aac6b02e44f4f95dc5cbfc99b810df468
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 11:17:32 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *jack_bauer - print all minutes of the day, format 00:00
+ *@void: Null value
+ *
+ *@h: hours
+ *@m: minutes
+ * Return: the hours of the complete day
+*/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+		for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h % 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m % 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
