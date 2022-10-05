d152d53532e3f6e00c5869fa8b5983d119010457
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 02:07:16 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,16 +7,17 @@
  * Return: Zero value
 */
 
+void print_alphabet(void);
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar (i);
+	_putchar ('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
-	{	char i;
-
-		for (i = 'a' ; i <= 'z' ; i++)
-			_putchar (i);
-
-		_putchar ('\n');
-	}
 
 	return (0);
 }
