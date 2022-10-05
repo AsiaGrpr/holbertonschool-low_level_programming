58295d4e49f5bd5ae013822ae32941eae48a1cab
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-05 05:43:09 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_islower - check if a letter is lowercase
+ *@void: Null value
+ *
+ * Return: Zero value
+*/
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= a && c <= z)
+	{ 
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
