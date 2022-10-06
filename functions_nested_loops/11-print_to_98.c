5b9575e174814a660b8469c8a1a52f94538c36b2
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-06 06:08:54 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,18 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 		while (n < 98)
 		{
 			printf ("%d, ", n);
 			++n;
 		}
-	else if (n > 98)
+	else if (n >= 98)
 		while (n > 98)
 		{
 			printf ("%d, ", n);
 			--n;
 		}
-	else
-		printf ("%d", n);
 
-	printf ("\n");
+	printf ("98\n");
 }
