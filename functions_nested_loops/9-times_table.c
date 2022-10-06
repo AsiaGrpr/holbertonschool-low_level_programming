141d11e1ea27757d446763c5ddb613f9b3aa069d
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-06 06:49:34 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *times_table - print the 9 tables
+ *
+ *
+ * Return: Null value
+*/
+void times_table(void)
+{
+	int x, y, p;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			p = x * y;
+		}
+	}
+}
