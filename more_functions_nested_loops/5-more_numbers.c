7845e62baaa57642e46f3b40fde3cd7741dcd7cb
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-07 02:50:32 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *@void: Null value
+ *
+ * Return: Null value
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+	int a;
+
+	for (a = 0 ; a < 14 ; a++)
+		_putchar(a + '0');
+
+	_putchar('\n');
+	}
+}
