226198d025570ff37cd6875933472ee966bd4bf7
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-07 05:18:52 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - draws a straight line in the terminal
+ *@n: number of character '_' to draw
+ *
+ * Return: Zero value
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0 && n < 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(95);
+			_putchar('\n');
+		}
+	}
+}
