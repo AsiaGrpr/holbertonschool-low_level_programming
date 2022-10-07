3e871a7e2509e745e7486c40d933b3140dc18bb2
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-07 07:25:05 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *FizzBuzz - print each number from 1 to 100
+ *multiples of 3 printed Fizz instead
+ *multiples of 5 printed Buzz instead
+ *etc
+ */
+int i;
+
+for (i = 0; i < 100; i++)
+{
+	if ((i % 3) = 0)
+	{
+		printf("Fizz");
+	}
+	else if ((i % 5) = 0)
+	{
+		printf("Buzz");
+	}
+	else if ((i % 3) = 0 && (i % 5) = 0)
+	{
+		printf("FizzBuzz")
+	}
+	printf("\n");
+}
