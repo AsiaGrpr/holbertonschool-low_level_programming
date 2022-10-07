f9ac21828e0ac242710a50836fb5e6c387731a84
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-07 01:42:03 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - make multiplacation with two integers
+ *@a: number to use
+ *@b: number to use
+ *
+ *Return: Value of the multiplication
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}
