f29c7b6c58b16c53ecfd8e8b8e0262cb9359dce5
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-08 12:24:04 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -22,13 +22,13 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (i == 100)
-		{
-			printf("Buzz\n");
 		}
 		else
 		{
