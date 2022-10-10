f40084b44c8a70813145423da196fc9cbc1c75b9
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-10 07:01:09 -0700

    0-reset_to_98.c corrected

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *reset_to_98 - update the valu of n to 98
- *@*n: value of n
+ *reset_to_98 - update the value of n to 98
+ *@n: int to update
  *
  * Return: Null value
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int * n = &n;
+	int *n = n;
 	int *n = 402;
 
 	_putchar(n + '0');
