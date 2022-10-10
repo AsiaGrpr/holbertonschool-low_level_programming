3f308bb26c52c990e4a72bd0769308e6743b8e36
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-10 07:17:00 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,11 @@
 
 /**
  *reset_to_98 - update the value of n to 98
- *@n: int to update
+ *@n: value to update
  *
  * Return: Null value
  */
-void reset_to_98(int n)
+void reset_to_98(int *n)
 {
-	int n;
-	int *n = n;
 	int *n = 402;
-
-	_putchar(n + '0');
 }
