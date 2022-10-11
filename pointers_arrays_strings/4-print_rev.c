f511fe5320c1cefbeaff1e1e5bdd1b0279cdd44a
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-11 05:08:55 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_rev - Write a function that prints a string, in reverse
+ *@s: string to stdout
+ *
+ * Return: Null value
+ */
+void print_rev(char *s)
+{
+	int i;
+	int c = 0;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c++;
+	}
+
+	for (i = c -1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
