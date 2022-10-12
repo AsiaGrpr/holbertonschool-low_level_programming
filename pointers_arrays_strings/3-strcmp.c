9716d2afa5deb192c7b4ce24a3db36d4ac3503cd
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-12 08:58:26 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,34 +5,22 @@
  *@s1: first string
  *@s2: second string to compare
  *
- * Return: 0 for equality, -1 if s1<s2 and 1 if s1>s2
+ * Return: 0 for equality, negative int if s1<s2 and positive int if s1>s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (*s1 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
 
-	while (*s2 != '\0')
-	{
-		j++;
-	}
-
-	if (i == j)
+	if (*s1 == *s2)
 	{
 		return (0);
 	}
-	else if (i < j)
+	else
 	{
-		return (i - j);
+		return (*s1 - *s2);
 	}
-	else if (i > j)
-	{
-		return (i - j);
-	}
-	return (0);
 }
