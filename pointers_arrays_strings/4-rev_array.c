8bbfe89641f8d05c0b946ff71d18e6b0c3dee791
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-14 01:09:13 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *reverse_array - Write a function that reverses the content of an array of integers
+ *@a: the array
+ *@n: number of element of the array
+ *
+ * Return: pointer to the resulting string
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+
+	if ((n % 2) == 0)
+		for (i = 0; i < (n / 2); i++)
+		{
+			int tmp = *(a + ((n - 1) - i));
+			*(a + (n - i)) = *(a + i);
+			*(a + i) = tmp;
+		}
+	if ((n % 2) != 0)
+		for (i = 0; i < ((n / 2) + 1); i++)
+		{
+			int tmp = *(a + ((n - 1) - i));
+			*(a + (n - i)) = *(a + i);
+			*(a + i) = tmp;
+		}
+}
+
