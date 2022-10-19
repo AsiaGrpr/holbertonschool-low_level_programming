72791e3c0bfda692e08b509f66a41f9b13ea1628
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-19 06:47:25 -0700

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - multiply 2 numbers
+ *@argc: number of the argument
+ *@argv: string of command line
+ *
+ *Return: 0 if it works or 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
