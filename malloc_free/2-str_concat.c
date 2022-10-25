31bc13ce5a09a79357027736a8c44466462d9290
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-25 05:32:47 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
