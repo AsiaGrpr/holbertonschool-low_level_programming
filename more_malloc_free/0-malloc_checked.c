f0bccb1b98ec2c256f8e25b402e09ec330830637
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-27 03:09:56 -0700

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: unsigned int
+ *
+ * Return: NULL value
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	(void)p;
+
+	return (p);
+}
