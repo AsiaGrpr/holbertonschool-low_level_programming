834d78baae8014e5df6117a7baf97609e2d17342
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-27 03:54:29 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - fonction to allocate memory for an array with malloc
+ * @nmemb: size of the array
+ * @size: amount of bytes
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size * nmemb; ++i)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
