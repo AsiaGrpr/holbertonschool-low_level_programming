c054578b51ce6d8d7093f6e4fb78e7dd65d2c224
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-31 06:43:13 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,16 +9,14 @@
  *
  * Description: This structure will compile informations about dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-
 
 #endif
