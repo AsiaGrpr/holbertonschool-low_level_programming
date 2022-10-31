b18e73c6042b3cdd927124b3d5907a8fcf23ee33
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-31 02:15:41 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - name, age, owner informations
+ * @name: char * member
+ * @age: float member
+ * @owner: char * member
+ *
+ * Description: This structure will compile informations about dog
+ */
+
+#define dog 
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
