9d5cb2011ededab6d91ee37243a1688b129eca92
AsiaGrpr <groupierre.asia@gmail.com>
2022-10-31 04:19:29 -0700

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,10 +10,16 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL || d->age == 0.0 || d->owner == NULL)
-		printf("(nil)");
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL || d->age == 0.0 || d->owner == NULL)
+			printf("(nil)");
+	
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
+	}
 }
