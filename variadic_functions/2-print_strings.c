6cf91e2a993a4b5df98d663461b821c4923c7583
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-02 04:47:45 -0700

    2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings
+ * @separator: char to separate the different elements
+ * @n: number of elements  to print
+ *
+ * Return: NULL value
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *p;
+
+	va_start(list, n);
+
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			p = va_arg(list, char*);
+
+			if (list != NULL)
+				printf("%s", p);
+
+			else
+				printf("(nil)");
+
+			if (i < (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(list);
+		printf("\n");
+	}
+	else
+		printf("\n");
+}
