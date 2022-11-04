73b17abaa73a401d0947c184bc0a92f2059aeb20
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-04 02:08:07 -0700

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: num1
+ * @b: num2
+ *
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: num1
+ * @b: num2 to substract
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
