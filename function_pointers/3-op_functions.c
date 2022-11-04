a9a4f963cbb15225ad96fb820524534d34b8cf99
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-04 02:50:28 -0700

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -24,4 +24,39 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 
+/**
+ * op_mul - returns the product of a and b
+ * @a: num1
+ * @b: num2
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the div of a by b
+ * @a: num1 to divide
+ * @b: num2
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the modulo of a by b
+ * @a: num1
+ * @b: num2
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
 
