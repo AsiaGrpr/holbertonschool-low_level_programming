e1aac2987346051b7767285827b0088d9cb22c47
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-15 04:12:31 -0800

    5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list_t list
+ * @head: pointer to pointer to head of linked list
+ *
+ * Return: Void value
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head;
+	listint_t *next;
+
+	while (node)
+	{
+		next = node->next;
+		free(node);
+		node = next;
+	}
+
+	*head = NULL;
+}
