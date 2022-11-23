670704aaee3d3613b599bd3ea56902d4589d9f21
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-23 02:03:28 -0800

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table or NULL if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL)
+
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
+
+	return (table);
+}
