41c45876ee1048a62b1b42350c15bc479be3bbff
AsiaGrpr <groupierre.asia@gmail.com>
2022-11-24 04:58:34 -0800

    3-hash_table_set.c correction malloc return

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -49,17 +49,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *valuedup;
 	hash_node_t *node = NULL, *new = NULL;
 
-	index = key_index((unsigned char *)key, ht->size);
-
 	if (!key || strcmp(key, "") == 0 || !ht || !value)
 		return (0);
 
+	index = key_index((unsigned char *)key, ht->size);
+
 	valuedup = strdup(value);
 	if (!valuedup)
-	{
-		free(node);
 		return (0);
-	}
 
 	node = ht->array[index];
 
