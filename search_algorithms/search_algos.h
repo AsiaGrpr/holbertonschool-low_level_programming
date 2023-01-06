94d8a7fa709abbe8ef9859fce823aafa8484d33b
AsiaGrpr <groupierre.asia@gmail.com>
2023-01-06 07:31:44 -0800

    search_algos.h corrections

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,5 +1,8 @@
-#ifndef SEARCH_ALGO.H
-#define SEARCH_ALGO.H
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
